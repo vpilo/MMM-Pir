4a77bf5eaa93fcd9903166b10d859fb94790f683
Bugsounet - Cédric <github@bugsounet.fr>
2025-01-14 17:29:51 +0100

    Create update.cmd (windows updater)

---
+++ installer/update.cmd
@@ -0,0 +1,38 @@
+@echo off
+
+call :welcome
+call :clean
+call :pull
+call :install
+exit /b
+
+:welcome
+echo.
+echo Welcome to MMM-Pir updater
+echo ----
+echo.
+exit /b
+
+:clean
+echo.
+echo Cleaning MMM-Pir Core
+echo ----
+echo.
+call npm run reset:windows
+exit /b
+
+:pull
+echo.
+echo Update MMM-Pir core
+echo ----
+echo.
+call git pull
+exit /b
+
+:install
+echo.
+echo Setup MMM-Pir
+echo ----
+echo.
+call npm run setup:windows
+exit /b
