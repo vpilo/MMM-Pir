8dad55883a82c1c854c73998bc203932073c03ef
Bugsounet - Cédric <github@bugsounet.fr>
2025-01-14 17:32:27 +0100

    Update windows.cmd

--- installer/windows.cmd
+++ installer/windows.cmd
@@ -1,20 +1,39 @@
 @echo off
 
+call :welcome
 call :node
 call :deps
 call :src
 exit /b
 
+:welcome
+echo.
+echo Welcome to MMM-Pir installer
+echo ----
+echo.
+exit /b
+
 :node
+echo.
 echo Remove node-libgpiod library (not compatible)
+echo ----
+echo.
 call npm remove node-libgpiod
 exit /b
 
-:deps 
+:deps
+echo.
 echo Install Dependencies
+echo ----
+echo.
 call npm prune
 exit /b
 
 :src
+echo.
+echo Install src files
+echo ----
+echo.
 call cd installer && node minify
+echo.
 exit /b
