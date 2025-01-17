5f9fdadb9d6c1bac7e213fdd2fa8f47415cc0d83
bugsounet <github@bugsounet.fr>
2025-01-17 15:28:21 +0100

    add step 2 and 3

--- installer/setup.js
+++ installer/setup.js
@@ -1,15 +1,112 @@
 const utils = require("./utils");
+const packageJSON = require("../package.json");
 
 async function main () {
   // Let's start !
+  utils.empty();
   utils.info(`Welcome to ${utils.moduleName()} v${utils.moduleVersion()}`);
   utils.empty();
-  utils.info("① Checking OS...")
-  let sysinfo = await utils.checkOS();
-  console.log(sysinfo);
+  await checkOS()
   utils.empty();
-  utils.info("② ➤ dependencies installer");
+}
+
+async function checkOS () {
+  utils.info("① ➤ Checking OS...");
+  const sysinfo = await utils.checkOS();
+  switch (sysinfo.type) {
+    case "Linux":
+      utils.success(`OS Detected: Linux (${sysinfo.name} ${sysinfo.version} ${sysinfo.arch})`);
+      await updatePackageInfoLinux();
+      await installLinuxDeps()
+      console.log("done")
+      break
+    case "Darwin":
+      utils.error(`OS Detected: Darwin (${sysinfo.name} ${sysinfo.version} ${sysinfo.arch})`);
+      utils.error("Automatic installation is not included");
+      utils.empty();
+      process.exit();
+      break;
+    case "Windows":
+      utils.success(`OS Detected: Windows (${sysinfo.name} ${sysinfo.version} ${sysinfo.arch})`);
+      installWindowsDeps();
+      break;
+  }
+  return
+}
+
+function updatePackageInfoLinux () {
   utils.empty();
+  utils.info("② ➤ Update package informations");
+  utils.empty();
+  return new Promise ((resolve) => {
+    utils.update((err) => {
+      if (err) {
+        utils.error("Error Detected!")
+        process.exit()
+      }
+      resolve();
+    })
+      .on('stdout', function(data) {
+        utils.out(data.trim());
+      })
+      .on('stderr', function(data) {
+        utils.error(data.trim())
+      });
+  })
+}
+
+async function installLinuxDeps () {
+  utils.empty();
+  utils.info("③ ➤ Dependencies installer");
+  utils.empty();
+  const apt = packageJSON.apt
+  if (!apt || typeof apt === "string") {
+    utils.out("No dependecies needed!")
+    return
+  }
+  if (!Array.isArray(apt)) {
+    utils.error("apt format Error!")
+    return
+  }
+  if (!apt.length) {
+    utils.out("No dependecies needed!")
+    return
+  }
+
+  return new Promise (async (resolve) => {
+    var modulesToInstall = await utils.check(apt)
+    modulesToInstall = modulesToInstall.toString().replace(",", " ")
+    utils.install(modulesToInstall, (err) => {
+      if (err) {
+        utils.error("Error Detected!")
+        process.exit()
+      }
+      resolve()
+    })
+      .on('stdout', function(data) {
+        utils.out(data.trim());
+      })
+      .on('stderr', function(data) {
+        utils.error(data.trim())
+      });
+  })
+}
+
+async function installWindowsDeps () {
+  utils.empty();
+  utils.info("② ➤ Dependencies installer");
+  utils.empty();
+  
+    utils.show("curl", (err,data) => { console.log("--->", err, data)})
+  utils.install("python3", (err,data) => { console.log("--->", err, data)})
+    .on('stdout', function(data) {
+      utils.out(data.trim());
+    })
+    .on('stderr', function(data) {
+      utils.error(data.trim())
+    });
+  
+  return
 }
 
 main();
--- installer/utils.js
+++ installer/utils.js
@@ -2,8 +2,9 @@ const os = require("node:os");
 
 const util = require("node:util");
 const packageJSON = require("../package.json");
-const exec = util.promisify(require("node:child_process").exec);
-
+const Exec = util.promisify(require("node:child_process").exec);
+var exec = require('child_process').exec;
+var events = require('events');
 
 // color codes
 const reset = "\x1B[0m";
@@ -19,117 +20,215 @@ function message (text, color) {
   console.log(`${color}${text}${reset}`);
 }
 
-module.exports = {
-  // Display an empty line
-  empty () {
-    message("", reset);
-  },
-
-  // Display question in cyan
-  question (text) {
-    message(text, cyan);
-  },
-
-  // Display a error in red
-  error (text) {
-    message(text, red);
-  },
-
-  // Display a warning in yellow
-  warning (text) {
-    message(text, orange);
-  },
-
-  // Display a success in green
-  success (text) {
-    message(text, green);
-  },
-
-  // Display out std message in gray
-  out (text) {
-    message(text, gray);
-  },
-
-  // Display an information in blue
-  info (text) {
-    message(text, blue);
-  },
-
-  // Display module Name
-  moduleName () {
-    return packageJSON.name || "Unknow";
-  },
-
-  // Display module Version
-  moduleVersion () {
-    return packageJSON.version || "X.Y.Z";
-  },
-
-  // Display module rev date
-  moduleRev () {
-    return packageJSON.Rev || "------";
-  },
-
-  // checkOS
-
-  async execCMD (command) {
-    const { stdout, stderr } = await exec(command);
-    if (stderr) return "Error";
-    else return stdout.trim();
-  },
-
-
-  async checkOS () {
-    let type = os.type();
-    let sysinfo = {
-      type: null,
-      arch: null,
-      name: null,
-      version: null
-    };
-
-    switch (type) {
-      case "Linux":
-        sysinfo.type = "Linux";
-        sysinfo.arch = await this.execCMD("uname -m");
-        sysinfo.name = await this.execCMD("cat /etc/*release | grep ^ID= | cut -f2 -d=");
-        sysinfo.version = await this.execCMD("cat /etc/*release | grep ^VERSION_ID= | cut -f2 -d= | tr -d '\"'");
-        return sysinfo;
-      case "Darwin":
-        sysinfo.type = "Darwin";
-        sysinfo.arch = await this.execCMD("uname -m");
-        sysinfo.name = await this.execCMD("sw_vers -productName");
-        sysinfo.version = await this.execCMD("sw_vers -productVersion");
-        return sysinfo;
-      case "Windows_NT":
-        sysinfo.type = "Windows";
-        return sysinfo;
-      default:
-        sysinfo.type = undefined;
-        return sysinfo;
-    }
-  }
-
-  /*
-  case "$OSTYPE" in
-    linux*)   platform="linux"
-              arch="$(uname -m)"
-              os_name="$(cat /etc/*release | grep ^ID= | cut -f2 -d=)"
-              os_version="$(cat /etc/*release | grep ^VERSION_ID= | cut -f2 -d= | tr -d '"')"
-              ;;
-    darwin*)  platform="osx"
-              arch="$(uname -m)"
-              os_name="$(sw_vers -productName)"
-              os_version="$(sw_vers -productVersion)"
-              ;;
-    *)        Installer_error "$OSTYPE is not a supported platform"
-              exit 0;;
-  esac
+// Display an empty line
+function empty () {
+  message("", reset);
 }
-*/
-  /*
-  isWin () {
-    return process.platform === "win32";
-  },
-  */
+
+// Display question in cyan
+function question (text) {
+  message(text, cyan);
+}
+
+// Display a error in red
+function error (text) {
+  message(text, red);
+}
+
+// Display a warning in yellow
+function warning (text) {
+  message(text, orange);
+}
+
+// Display a success in green
+function success (text) {
+  message(text, green);
+}
+
+// Display out std message in gray
+function out (text) {
+  message(text, gray);
+}
+
+// Display an information in blue
+function info (text) {
+  message(text, blue);
+}
+
+// Display module Name
+function moduleName () {
+  return packageJSON.name || "Unknow";
+}
+
+// Display module Version
+function moduleVersion () {
+  return packageJSON.version || "X.Y.Z";
+}
+
+// Display module rev date
+function moduleRev () {
+  return packageJSON.Rev || "------";
+}
+
+// checkOS
+
+async function execCMD (command) {
+  const { stdout, stderr } = await Exec(command);
+  if (stderr) return "Error";
+  else return stdout.trim();
+}
+
+async function checkOS () {
+  let type = os.type();
+  let sysinfo = {
+    type: null,
+    arch: null,
+    name: null,
+    version: null
+  };
+
+  switch (type) {
+    case "Linux":
+      sysinfo.type = "Linux";
+      sysinfo.arch = await execCMD("uname -m");
+      sysinfo.name = await execCMD("cat /etc/*release | grep ^ID= | cut -f2 -d=");
+      sysinfo.version = await execCMD("cat /etc/*release | grep ^VERSION_ID= | cut -f2 -d= | tr -d '\"'");
+      return sysinfo;
+    case "Darwin":
+      sysinfo.type = "Darwin";
+      sysinfo.arch = await execCMD("uname -m");
+      sysinfo.name = await execCMD("sw_vers -productName");
+      sysinfo.version = await execCMD("sw_vers -productVersion");
+      return sysinfo;
+    case "Windows_NT":
+      sysinfo.type = "Windows";
+      return sysinfo;
+    default:
+      sysinfo.type = undefined;
+      return sysinfo;
+  }
+}
+
+module.exports.empty = empty
+module.exports.question = question
+module.exports.error = error
+module.exports.warning = warning
+module.exports.success = success
+module.exports.out= out
+module.exports.info = info
+module.exports.moduleName = moduleName
+module.exports.moduleVersion= moduleVersion
+module.exports.moduleRev= moduleRev
+module.exports.checkOS = checkOS
+
+/* apt tools */
+
+var _path = {};
+
+/**
+ * Get or set the path for a binary alias
+ */
+var path = module.exports.path = function(alias, path) {
+    if (path) {
+        _path[alias] = path;
+    }
+    return _path[alias] || alias;
 };
+
+/**
+ * check the currently installed of the module using dpkg -s
+ * return: null if installed or err if not installed
+ */
+function checker (name, callback) {
+    exec(`dpkg -s ${name}`, function(err, stdout, stderr) {
+        if (err) {
+            return callback(0);
+        }
+        return callback(1);
+    });
+};
+
+function check (names) {
+  var modules = []
+  return new Promise(resolve => {
+    names.forEach((name,i) => {
+      checker(name, (result) => {
+        if (result === 0) modules.push(name)
+        if (i === names.length-1) resolve(modules)
+      })
+    })
+  })
+};
+
+/**
+ * Update the apt cache using apt-get update
+ */
+function update(callback = () => {}) {
+    var emitter = new events.EventEmitter();
+    var child = exec('sudo apt-get update', function(err, stdout, stderr) {
+        if (err) {
+            return callback(err);
+        }
+
+        return callback();
+    });
+
+    child.stdout.on('data', function(data) {
+        emitter.emit('stdout', data);
+    });
+
+    child.stderr.on('data', function(data) {
+        emitter.emit('stderr', data);
+    });
+
+    return emitter;
+};
+
+/**
+ * Install the module with the given names
+ *
+ * @param   {String}    names                names of the modules to install
+ */
+function install(names, callback) {
+    var emitter = new events.EventEmitter();
+    var child = exec(`sudo apt-get install -y ${names}`, function(err, stdout, stderr) {
+        if (err) {
+            return callback(err);
+        }
+
+        return callback();
+    });
+
+    child.stdout.on('data', function(data) {
+        emitter.emit('stdout', data);
+    });
+
+    child.stderr.on('data', function(data) {
+        emitter.emit('stderr', data);
+    });
+
+    return emitter;
+};
+
+/**
+ * Uninstall the package with the given name
+ */
+function uninstall(name, callback) {
+    var emitter = new events.EventEmitter();
+    var child = exec(util.format('%s remove -y %s', path('apt-get'), name), callback);
+
+    child.stdout.on('data', function(data) {
+        emitter.emit('stdout', data);
+    });
+
+    child.stderr.on('data', function(data) {
+        emitter.emit('stderr', data);
+    });
+
+    return emitter;
+};
+
+module.exports.check = check
+module.exports.update = update
+module.exports.install = install
+module.exports.uninstall = uninstall
